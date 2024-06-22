d772e19b703751f3cf420d391671161f5f069696
Mehmet Şirin Usanmaz <mehmet.usanmaz@turk.net>
2024-06-22 10:49:04 +0300

    added windows headless support

--- src/main/java/com/musanmaz/drivers/DriverFactory.java
+++ src/main/java/com/musanmaz/drivers/DriverFactory.java
@@ -3,6 +3,7 @@ package com.musanmaz.drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -11,6 +12,12 @@ public class DriverFactory {
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
                 return new ChromeDriver();
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
